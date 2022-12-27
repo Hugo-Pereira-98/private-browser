3bb0deaa1c66ee1eeef2e7a3af52f74d1e5527a4
Hugo Pereira <hugop868@gmail.com>
2022-12-27 14:12:36 -0300

    styling elements

--- browser.py
+++ browser.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+
 
 class Browser(QWidget):
     def __init__(self):
@@ -9,7 +11,7 @@ class Browser(QWidget):
 
         # Create the QWebEngineView
         self.view = QWebEngineView(self)
-        self.view.load(QUrl("https://www.example.com"))
+        self.view.load(QUrl("https://www.google.com"))
 
         # Create the address bar
         self.address_bar = QLineEdit(self)
@@ -18,10 +20,13 @@ class Browser(QWidget):
         # Create the back, forward, and refresh buttons
         self.back_button = QPushButton("<", self)
         self.back_button.clicked.connect(self.view.back)
+        self.back_button.setIcon(QIcon("back.png"))
         self.forward_button = QPushButton(">", self)
         self.forward_button.clicked.connect(self.view.forward)
+        self.forward_button.setIcon(QIcon("forward.png"))
         self.refresh_button = QPushButton("⟳", self)
         self.refresh_button.clicked.connect(self.view.reload)
+        self.refresh_button.setIcon(QIcon("refresh.png"))
 
         # Create the layout
         layout = QVBoxLayout()
@@ -34,9 +39,30 @@ class Browser(QWidget):
         layout.addWidget(self.view)
         self.setLayout(layout)
 
+        # Set the stylesheet
+        self.setStyleSheet('''
+            QLineEdit {
+                border: 1px solid #999;
+                border-radius: 3px;
+                padding: 5px;
+                font-size: 16px;
+            }
+            QPushButton {
+                border: 1px solid #999;
+                border-radius: 3px;
+                background-color: #a9a9a9;
+                font-size: 16px;
+                padding: 5px;
+            }
+            QPushButton:hover {
+                background-color: #000000;
+            }
+        ''')
+
     def load_url(self):
         url = QUrl(self.address_bar.text())
         self.view.load(url)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
